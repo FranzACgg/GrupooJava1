da782fc08e8118cabd41cc3afa97ff288425dec8
FranzACgg <facevedo@uade.edu.ar>
2026-08-10 17:13:01 -0300

    Add files via upload

---
+++ Ejercicios/EjerciciosClase1_10_08_ejercicio3.java
@@ -0,0 +1,44 @@
+
+
+public class EjerciciosClase1_10_08_ejercicio3 {
+	
+	public static void main(String[] args){
+		
+		
+		int año= 2012;
+		
+		if (año%4==0) {
+			if (año%100==0){
+				if (año%400==0) {
+					System.out.println("El año es bisiesto");
+				}else {
+					System.out.println("El año no es bisiesto");
+				}
+			}else {
+				System.out.println("El año no es bisiesto");
+			}
+		}else {
+			System.out.println("El año no es bisiesto");
+		}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		
+}
+	
+
